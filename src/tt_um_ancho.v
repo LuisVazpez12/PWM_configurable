222333c6d65fe49f7859abfaaf0079647654969e
LuisVazpez12 <165457387+LuisVazpez12@users.noreply.github.com>
2024-03-31 01:28:59 -0600

    Update and rename project.v to tt_um_ancho.v

--- src/tt_um_ancho.v
+++ src/tt_um_ancho.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_ancho (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,7 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
+ancho # c1(.clock(ui_in[7]), .enable(ui_in[6]),.speed(ui_in[5:3]) .PWM(uo_out[0]));
+assign uo_out[7:1]=7*b0000000
+assign uio_out[7:0]=8*b00000000
 endmodule
