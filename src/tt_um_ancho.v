ad2bd22336505fd78966a39c445159caa9fe2aea
LuisVazpez12 <165457387+LuisVazpez12@users.noreply.github.com>
2024-04-01 12:37:38 -0600

    Update tt_um_ancho.v

--- src/tt_um_ancho.v
+++ src/tt_um_ancho.v
@@ -18,4 +18,5 @@ module tt_um_ancho (
     ancho # c1(.clock(ui_in[7]), .enable(ui_in[6]), .speed(ui_in[5:3]), .PWM(uo_out[0]));
     assign uo_out[7:1]=7'b0000000;
     assign uio_out[7:0]=8'b00000000;
+    assign uio_oe[7:0] = 8'b00000000;
 endmodule
