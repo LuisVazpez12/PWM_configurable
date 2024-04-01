aaa1afc0963ff8ce131b000231cf6e5a11d21b71
LuisVazpez12 <165457387+LuisVazpez12@users.noreply.github.com>
2024-03-31 23:20:55 -0600

    Actualizar tt_um_ancho.v

--- src/tt_um_ancho.v
+++ src/tt_um_ancho.v
@@ -16,7 +16,7 @@ module tt_um_ancho (
     input  wire       rst_n     // reset_n - low to reset
 );
 PAncho
-( .clock(ui_in[7]),.enable(ui_in[6]),.speed(ui_in[5:3]),.PWM(uo_out[7]));
+#( .clock(ui_in[7]),.enable(ui_in[6]),.speed(ui_in[5:3]),.PWM(uo_out[7]));
     assign uo_out [6:0]= 7'b0000000;
     assign uio_out [7:0] = 8'b00000000;
 endmodule
